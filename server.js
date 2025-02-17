85833d290bc4fd4254ab6211031cf9bab896c272
GabrielReyes3 <119997410+GabrielReyes3@users.noreply.github.com>
2025-02-17 17:23:36 -0600

    First commit
    blahblahblah

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
